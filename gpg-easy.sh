4baf21c6104ff283babdf7a17e0d66b18226dae3
Tilley <tilleyy8@gmail.com>
2024-12-08 14:25:13 -0700

    Update gpg-easy.sh

--- gpg-easy.sh
+++ gpg-easy.sh
@@ -1,45 +1,40 @@
-function create () {
-    clear
-    gpg --gen-key
-}
+#!/bin/bash
 
-function toclipboard () {
-    if [ $(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}') == "x11" ]; then
-        echo $toclipboard | xclip -selection clipboard
-    elif [ $(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}') == "wayland" ]; then
-        echo $toclipboard | wl-copy
-    fi
-}
+choice=$(gum choose "create key" "import key" "encrypt message" "decrypt message")
 
-function fromclipboard () {
-    if [ $(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}') == "x11" ]; then
-        fromclipboard=$(xclip -o -selection clipboard)
-    elif [ $(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}') == "wayland" ]; then
-        fromclipboard=$(wl-paste)
-    fi
-}
-
-function import () {
-    touch /tmp/westorekeyshere.gpg
-    rm /tmp/westorekeyshere.gpg
-    echo $fromclipboard >> /tmp/westorekeyshere.gpg
-    gpg --import /tmp/westorekeyshere.gpg
-}
-
-choice=$(gum choose "create-key" "import" "encrypt" "decrypt")
 case "$choice" in
-"create-key")
-    create
+"create key")
+    gpg --gen-key
     ;;
-"import")
-    fromclipboard
-    echo $fromclipboard
-    import
+"import key")
+    read -rp "Enter the file name to import: " import_file
+    gpg --armor --import "$import_file"
     ;;
-"export")
+"encrypt message")
+    read -rp "Enter the text to encrypt: " text
+    echo "Enter recipient names (type 'q' to finish):"
+    recipients=()
+    while read -rp "Recipient: " recipient && [ "$recipient" != "q" ]; do
+        recipients+=("--recipient" "$recipient")
+    done
+    encrypted_text=$(echo "$text" | gpg --sign --armor --encrypt "${recipients[@]}" 2>/dev/null)
+    echo "$encrypted_text" | wl-copy --type text/plain
+    echo "Encrypted text has been copied to the clipboard."
     ;;
-"encrypt")
+"decrypt message")
+    if wl-paste | grep -q "BEGIN PGP MESSAGE"; then
+        decrypted_text=$(wl-paste | gpg --armor --decrypt 2>/dev/null)
+        if [ $? -eq 0 ]; then
+            print "$decrypted_text" | wl-copy --type text/plain
+            echo "Decrypted text has been copied to the clipboard."
+        else
+            echo "Decryption failed."
+        fi
+    else
+        echo "No PGP message found in the clipboard."
+    fi
     ;;
-"decrypt")
+*)
+    echo "Invalid choice."
     ;;
 esac
