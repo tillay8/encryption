19ee26ecbafd42802f903185a1ba09008ab50257
Tilley <tilleyy8@gmail.com>
2024-12-10 10:09:50 -0700

    reenable @@ functionality

--- random-password.py
+++ random-password.py
@@ -1,15 +1,9 @@
 import random, string, os, subprocess
-
 length = 30
 characters = string.ascii_letters + string.digits + string.punctuation
-password = (''.join(random.choices(characters, k=length)))
-
-try:
-    if "WAYLAND_DISPLAY" in os.environ: 
-        subprocess.run(["wl-copy"], input=password, text=True, check=True)
-    elif "DISPLAY" in os.environ:
-        subprocess.run(["xclip", "-selection", "clipboard"], input=password, text=True, check=True)
-except Exception as e:
-    print("ERRORRRR: skill issue")
-
+password = ("@@"+''.join(random.choices(characters, k=length)))
+if "WAYLAND_DISPLAY" in os.environ: 
+    subprocess.run(["wl-copy"], input=password, text=True, check=True)
+elif "DISPLAY" in os.environ:
+    subprocess.run(["xclip", "-selection", "clipboard"], input=password, text=True, check=True)
 print("password copied to clip")
