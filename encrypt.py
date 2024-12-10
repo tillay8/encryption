d0ad79375894370e582f217427fed4f98d0931c6
Tilley <tilleyy8@gmail.com>
2024-12-10 12:05:43 -0700

    add new password functionality to encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,8 +1,19 @@
 from hashlib import sha256
 from Crypto.Cipher import AES # Please make sure the pycryptodome package is installed
 from Crypto.Util.Padding import pad, unpad
-import os, base64, subprocess, sys # Please be on a linux system and have wl-copy and xclip installed
-key_file_path = "/tmp/key" # this is designed to store in the ram on a linux system.
+import os, base64, subprocess, sys, random, string #  Please be on a linux system and have wl-copy and xclip installed
+key_file_path = "/tmp/key" # This is designed to store in the ram on a linux system. This path can be changed.
+def copy_to_clipboard(text): # This copies text to the clipboard for the specific desktop environment
+    subprocess.run(["wl-copy"], input=text.encode()) if os.environ.get("XDG_SESSION_TYPE") == "wayland" else subprocess.run(["xclip", "-selection", "clipboard"], input=text.encode())
+if (len(sys.argv) > 1): # This checks for a flag
+    if sys.argv[1] == "-n": # If flag is -n, delete the key file to manually change key
+        os.remove(key_file_path) if os.path.exists(key_file_path) else None
+    elif sys.argv[1] == "-p": # If flag is -p, create a new random password
+        password = ("@@"+''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=30))) # Create new password by splicing strings together
+        copy_to_clipboard(password) # Copy new password to clipboard. k on previous line is length of random password
+        with open(key_file_path, 'w') as key_file: key_file.write(password) # Overwrite the filepath with the new password
+        print(f"New password saved to {key_file_path} and copied to clipboard.")
+        sys.exit(0) # Finish script and indicate success
 clipboard_content = subprocess.getoutput("wl-paste" if os.environ.get("XDG_SESSION_TYPE") == "wayland" else "xclip -o -selection clipboard")
 def password_logic(): # Above line sets a varaible to the text on the clipboard before running the script
     if clipboard_content.startswith("@@"): # This checks if the text on the clipboard starts with "@@"
@@ -25,8 +36,6 @@ def decrypt(ciphertext, passphrase): # This attempts to decrypt the text accordi
         return unpad(cipher.decrypt(decoded_data[AES.block_size:]), AES.block_size).decode()
     except (ValueError, KeyError): # This detects if it is unable to decrypt the code with the password
         return None
-def copy_to_clipboard(text): # This copies text to the clipboard for the specific desktop environment
-    subprocess.run(["wl-copy"], input=text.encode()) if os.environ.get("XDG_SESSION_TYPE") == "wayland" else subprocess.run(["xclip", "-selection", "clipboard"], input=text.encode())
 def process_clipboard_content(content, password, copy=False):
     if content.startswith("&&"): # This detects if the text on the clipboard is encrypted and knows to decrypt it
         print("Decrypted text:", decrypt(content[2:], password) or "Incorrect password.")
@@ -34,7 +43,5 @@ def process_clipboard_content(content, password, copy=False):
     else:
         encrypted_text = "&&" + encrypt(input("Text Input: "), password) # This encrypts text and makes it start with "&&"
         (copy_to_clipboard(encrypted_text), print("Encrypted text copied to clipboard.")) if copy else print(encrypted_text)
-if (len(sys.argv) > 1 and sys.argv[1] == "-n"): # This checks for a flag to manually change the password
-    os.remove(key_file_path) if os.path.exists(key_file_path) else None
 password = password_logic() # This runs the code to check if the password exists and set it
 process_clipboard_content(clipboard_content, password, copy=True) # This runs the main process
