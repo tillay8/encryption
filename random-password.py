8560ef85f15910e7c5210d897bd1198d7f83a779
Tilley <tilleyy8@gmail.com>
2024-12-10 12:04:33 -0700

    Delete random-password.py

--- random-password.py
+++
@@ -1,9 +0,0 @@
-import random, string, os, subprocess
-length = 30
-characters = string.ascii_letters + string.digits + string.punctuation
-password = ("@@"+''.join(random.choices(characters, k=length)))
-if "WAYLAND_DISPLAY" in os.environ: 
-    subprocess.run(["wl-copy"], input=password, text=True)
-elif "DISPLAY" in os.environ:
-    subprocess.run(["xclip", "-selection", "clipboard"], input=password, text=True)
-print("password copied to clip")
